557bdca4fd277d52306da92bf0838244b4a56110
Yashwanth666 <codewithmee666@gmail.com>
2026-05-11 00:07:25 +0530

    Configure API base URL for Vercel deployment

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API_BASE = '/api';
+const API_BASE = import.meta.env.VITE_API_URL 
+  ? `${import.meta.env.VITE_API_URL}/api` 
+  : '/api'
 
 const api = axios.create({
   baseURL: API_BASE,
